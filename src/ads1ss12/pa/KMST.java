2715461d11b182a3b7722163e03a60f848825ea6
Lorenz Leutgeb <lorenz.leutgeb@posteo.de>
2016-06-07 15:58:42 +0200

    Comment in more detail

--- src/ads1ss12/pa/KMST.java
+++ src/ads1ss12/pa/KMST.java
@@ -4,10 +4,28 @@ import java.util.HashSet;
 import java.util.TreeMap;
 import java.util.Map;
 
-/** @author Lorenz Leutgeb */
+/**
+ * Computes the kMST of a given graph.
+ *
+ * For the sake of this task, running time is decidedly more important than
+ * comprehensibility. This code is ugly but fast.
+ *
+ * @author Lorenz Leutgeb
+ */
 public class KMST extends AbstractKMST {
+	/** Holds the edges of the graph we're looking at. */
 	private final Edge[] edges;
+
+	/**
+	 * Global buffer for when we need some memory to store a solution.
+	 * Normally, this should be a local variable, but we try to optimize here.
+	 */
 	private final Edge[] solution;
+
+	/**
+	 * Global buffer to store a selection of nodes.
+	 * Normally, this should be a local variable, but we try to optimize here.
+	 */
 	private boolean[] taken;
 
 	public KMST(Integer numNodes, Integer numEdges, HashSet<Edge> edges, int k) {
@@ -16,25 +34,31 @@ public class KMST extends AbstractKMST {
 		this.taken = new boolean[numNodes];
 	}
 
-	@Override public void run() {
-		// AVL-Tree to store Prim's results
-		// TODO: try a minimum heap with min() in O(1) instead of a
-		//       balanced tree (O(logn))
+	@Override
+	public void run() {
+		// TreeMap to store Prim's results ordered by their weight.
+		// TODO: Try a minimum heap with min() in O(1) instead of a
+		//       balanced tree (O(logn)).
 		Map<Integer, Integer> roots = new TreeMap<Integer, Integer>();
 		
 		Edge[] edges = new Edge[this.edges.length];
-		int n, weight, relevantEdges, root, lowerBound = 0;;
+		int n, weight, relevantEdges, root, lowerBound = 0;
 	
-		// sort edges by weight
+		// Sort edges by weight.
 		quickSort(0, this.edges.length - 1);
 	
-		// compute initial lower bound (best k - 1 edges)
+		// Compute initial lower bound (best k - 1 edges).
+		// Choosing the cheapest k - 1 edges is not very intelligent. There is no guarantee
+		// that this subset of edges even induces a subgraph over the initial graph.
+		// TODO: Find a better initial lower bound.
 		for (int i = 0; i < solution.length; i++) {
 			lowerBound += this.edges[i].weight;
 		}
 	
-		// iterate over all nodes in the graph and run Prim's algorithm
-		// until k - 1 edges are fixed
+		// Iterate over all nodes in the graph and run Prim's algorithm
+		// until k - 1 edges are fixed.
+		// As all induced subgraphs have k nodes and are connected according to Prim, they
+		// are candidate solutions and thus submitted.
 		for (root = 0; root < taken.length; root++) {
 			taken = new boolean[taken.length];
 			System.arraycopy(this.edges, 0, edges, 0, this.edges.length);
@@ -46,7 +70,7 @@ public class KMST extends AbstractKMST {
 
 			while (n < solution.length) { 
 				for (int i = 0; i < relevantEdges; i++) {
-					// XOR to check if connected & no circle
+					// XOR to check if connected and no circle.
 					if (taken[edges[i].node1] ^ taken[edges[i].node2]) {
 						taken[taken[edges[i].node1] ? edges[i].node2 : edges[i].node1] = true;
 						solution[n++] = edges[i];
@@ -54,15 +78,15 @@ public class KMST extends AbstractKMST {
 						System.arraycopy(edges, i + 1, edges, i, --relevantEdges - i);
 						break;
 					}
-					// check for circle
+					// Check for circle.
 					else if (taken[edges[i].node1]) {
 						System.arraycopy(edges, i + 1, edges, i, --relevantEdges - i);
 						break;
 					}
 				}
 			}
-			// sum up what we've just collected and submit this
-			// solution to the framework
+			// Sum up what we've just collected and submit this
+			// solution to the framework.
 			HashSet<Edge> set = new HashSet<Edge>(solution.length);
 			for (int i = 0; i < solution.length; i++) {
 				set.add(solution[i]);
@@ -70,7 +94,9 @@ public class KMST extends AbstractKMST {
 			setSolution(weight, set);
 			roots.put(weight, root);
 		}
-		// now for the real business, let's do some Branch-and-Bound
+
+		// Now for the real business, let's do some Branch-and-Bound. Roots of "k Prim Spanning Trees"
+		// are enumerated by weight to increase our chances to obtain the kMST earlier.
 		for (int item : roots.values()) {
 			taken = new boolean[taken.length];
 			System.arraycopy(this.edges, 0, edges, 0, this.edges.length);
@@ -79,12 +105,18 @@ public class KMST extends AbstractKMST {
 		}
 	}
 
+	/**
+	 * @param edges  the set of edges of the graph, ordered by weight
+	 * @param left   the number of nodes that still need to be fixed	
+	 * @param weight the weight of the 
+	 */
 	private void branchAndBound(Edge[] edges, int left, int weight, int lowerBound) {
 		Edge edge = null;
 		int tmp = -1, relevantEdges = edges.length;
 		for (int i = 0; i < relevantEdges; i++) {
 			edge = edges[i];
-			if (left == 0) { // finished!
+			if (left == 0) {
+				// No nodes are left to fix, we have reached a solution.
 				HashSet<Edge> set = new HashSet<Edge>(solution.length);
 				for (int j = 0; j < solution.length; j++) {
 					set.add(solution[j]);
@@ -93,29 +125,38 @@ public class KMST extends AbstractKMST {
 				return;
 			}
 			else if (weight + edge.weight > getSolution().getUpperBound()) {
-				// solution is too expensive, kill this branch
+				// The selection of edges so far plus the weight of the next edge
+				// exceeds the upper bound, so kill this branch.
 				return;
 			}
 			else if (taken[edge.node1] ^ taken[edge.node2]) {
+				// Choosing this edge does not break requirements of a tree.
+
+				// We choose this edge.
 				solution[solution.length - left] = edge;
+
+				// Find out which node was selected and indicate this in taken.
 				taken[(tmp = taken[edge.node1] ? edge.node2 : edge.node1)] = true;
 				
-				// remove edge for deeper recursions
+				// Remove edge for deeper recursions.
 				System.arraycopy(edges, i + 1, edges, i, relevantEdges-- - i-- - 1);
 				Edge[] copy = new Edge[relevantEdges];
 				System.arraycopy(edges, 0, copy, 0, relevantEdges);
 				
-				// recursive call
+				// Recursive call.
 				branchAndBound(copy, left - 1, weight + edge.weight, lowerBound);
-				
+
+				// Now unselect the node again.
 				taken[tmp] = false;
-				
+
 				if (i + left > relevantEdges) {
-					// not enough edges left
+					// Not enough edges left.
 					return;
 				}
 				else if ((lowerBound += (i < left - 1 ? edges[left - 1].weight : edges[i + 1].weight) - edge.weight) >= getSolution().getUpperBound()) {
-					// this branch will never reach a better solution than we've already found
+					// This branch will never reach a better solution than we've already found, because
+					// the lower bound minus the weight of the edge we selected plus the weight of the
+					// next best edge exceeds the upper bound.
 					return;
 				}
 			}
@@ -131,6 +172,7 @@ public class KMST extends AbstractKMST {
 			}
 			else {
 				if (i > left && i + 1 < relevantEdges) {
+					// This edge is skipped.
 					lowerBound -= edge.weight - edges[i + 1].weight;
 				}
 				if (lowerBound >= getSolution().getUpperBound()) {
